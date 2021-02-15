4dc6ad7e9c3784e6ef76677ab11629cf38d59326
manaslupisani <46645004+manaslupisani@users.noreply.github.com>
2021-02-15 15:03:29 +0100

    Refresh updater.sh
    Add a few variables, and update the command to fetch external ip

--- updater.sh
+++ updater.sh
@@ -1,2 +1,6 @@
-externalIp=$(curl icanhazip.com)
-echo url="https://username:password@domains.google.com/nic/update?hostname=domainToUpdate.com&myip=$externalIp" | curl -k -o ~/googleDNS/response.log -K -
+externalIp=$(dig +short myip.opendns.com @resolver1.opendns.com)
+username=<user>
+password=<pass>
+domainToUpdate=<your_domains>
+
+echo url="https://$username:$password@domains.google.com/nic/update?hostname=$domainToUpdate.com&myip=$externalIp" | curl -k -o ./response.log -K -
