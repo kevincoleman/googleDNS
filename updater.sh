effec45e5481be0410fa1617e4167fe09181e8d3
Theodore Ts'o <tytso@mit.edu>
2023-12-16 18:49:40 -0500

    Skip updating the IP address if it hasn't changed
    This avoids the number of updates that we send to Google DNS.
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- updater.sh
+++ updater.sh
@@ -15,5 +15,14 @@ fi
 
 externalIp=$($getIP)
 
+if test -s "$DIR/response.log" ; then
+    read old_status old_ip < "$DIR/response.log"
+    if test "$old_status" = "good" -o "$old_status" = "nochg" &&
+	    test "$old_ip" = "$externalIp" ; then
+	echo "IP address has not changed since the last update"
+	exit 0
+    fi
+fi
+
 touch "$LOGFILE"
 echo url="https://$username:$password@domains.google.com/nic/update?hostname=$domainToUpdate&myip=$externalIp" | curl -k -o "$LOGFILE" -K -
